492fa16a6494c4f0b5a91dae9de1f0d2ef0f7fb8
Gleb Tretyakov <glebx777@gmail.com>
2016-09-18 16:51:10 +0300

    created for future...

---
+++ libs/registration.js
@@ -0,0 +1,9 @@
+var log 	 	= require('./log')(module);
+var UserModel	= require('./mongo-db-manager').UserModel;
+
+
+function create_user(req, res) {
+	// TODO
+}
+
+module.exports.create_user = create_user;
